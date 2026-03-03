001930d0f256ba36677b53f5473ffa64635fe138
Usmankhalid20 <usmankhalid399354@gmail.com>
2026-03-03 23:41:58 +0500

    add app change

--- Mern_project_sturctrue/src/db/index.js
+++ Mern_project_sturctrue/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
        const connection = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-       console.log(`\ ln MongoDB connected !! Host: ${connection.connection.host} \n`);
+       console.log(`MongoDB connected !! Host: ${connection.connection.host} \n`);
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
--- Mern_project_sturctrue/src/index.js
+++ Mern_project_sturctrue/src/index.js
@@ -7,17 +7,24 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
 
-const app = express();
+// const app = express();
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`server running on port ${process.env.PORT || 8000}`);
+  }) 
+})
+.catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
+})
+// app.get("/", (req, res) => {
+//   res.send("API Running");
+// });
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running");
-});
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log("Server running");
+// });
 // import express from "express";
 
 // const app = express();
