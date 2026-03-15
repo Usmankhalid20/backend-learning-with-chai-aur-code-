983304835eb19898f95a187b0cdc5bb6c4fd49a6
Usmankhalid20 <usmankhalid399354@gmail.com>
2026-03-15 23:40:10 +0500

    Implement user and video models with mongoose schemas and plugins

---
+++ Mern_project_sturctrue/src/models/user.model.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unqiue: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+            email: {
+            type: String,
+            required: true,
+            unqiue: true,
+            lowercase: true,
+            trim: true,
+        },
+            fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String,  // cloudinary url 
+            required: true
+        },
+        coverImage: {
+            type: String,   // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        refreshToken: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.pre("save", async function (next) {
+    if(!this.ismodeified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+export const User = mongoose.model("User", userSchema);
