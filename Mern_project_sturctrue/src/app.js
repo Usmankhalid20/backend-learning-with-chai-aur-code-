7bb7c5ef333b528b0c25b2ef192f2aa1e7d7323f
Usmankhalid20 <usmankhalid399354@gmail.com>
2026-03-26 00:33:39 +0500

    add change

--- Mern_project_sturctrue/src/app.js
+++ Mern_project_sturctrue/src/app.js
@@ -12,8 +12,7 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static('public'));
 app.use(cookieParser());
-console.log("app is running...")
-console.log("hello world")
+console.log("app initialized")
 
 
 export { app }
