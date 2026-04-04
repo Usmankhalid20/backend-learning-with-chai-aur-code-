3fc8f26f4af3729ba363be0d72a4f47923392057
Usmankhalid20 <usmankhalid399354@gmail.com>
2026-04-04 16:58:24 +0500

    add error fixing in controller

--- Mern_project_sturctrue/src/controllers/user.controller.js
+++ Mern_project_sturctrue/src/controllers/user.controller.js
@@ -19,24 +19,32 @@ const registerUser = asyncHandler(async (req, res) => {
     $or: [{ email }, { username }],
   });
   if (existedUser) {
-    throw new ApiError('User already exists', 400);
+    throw new ApiError(409, "User with email or username already exists")
   }
   // AVATAR AND COVER IMAGE
   const avatarLocalPath = req.files?.avatar?.[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
 
+  let coverImageLocalPath;
+
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError('Avatar is required', 400);
-    console.log('DEBUG FILES:', req.files);
+    // console.log('DEBUG FILES:', req.files);
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   if (!avatar) {
-    throw new ApiError('Failed to upload avatar', 400);
+    throw new ApiError(400, 'Failed to upload avatar');
   }
+
+  
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-  console.log(req.files);
-  console.log(req.files?.avatar);
+  // console.log(req.files);
+  // console.log(req.files?.avatar);
+// console.log('DEBUG AVATAR:', avatar);
 
   const user = await User.create({
     fullname,
@@ -50,9 +58,9 @@ const registerUser = asyncHandler(async (req, res) => {
   const userCreated = await User.findById(user._id).select(
     '-password -refreshToken'
   );
-
+console.log('DEBUG USER CREATED:', userCreated);
   if (!userCreated) {
-    throw new ApiError('Failed to create user', 500);
+    throw new ApiError(500, 'Failed to create user');
   }
 
   return res
