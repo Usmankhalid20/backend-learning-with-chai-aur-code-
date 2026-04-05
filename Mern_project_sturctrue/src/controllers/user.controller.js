62e8b0de4fc6739a2359fa887cd429ec37c316d6
Usmankhalid20 <usmankhalid399354@gmail.com>
2026-04-05 23:41:14 +0500

    add login controller

--- Mern_project_sturctrue/src/controllers/user.controller.js
+++ Mern_project_sturctrue/src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
+const generateAccessAndRefershToken = async(userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken
+    await user.save({validateBeforeSave: false});
+
+    return {accessToken, refreshToken};
+  } catch (error) {
+    throw new ApiError(500, 'something went wrong while generating refersh and access tokens');
+  }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, fullname, password } = req.body;
 
@@ -50,7 +65,7 @@ const registerUser = asyncHandler(async (req, res) => {
     fullname,
     username: username.toLowerCase(),
     avatar: avatar.url,
-    coverImage: coverImage?.url,
+    coverImage: coverImage?.url || "",
     email,
     password,
   });
@@ -68,4 +83,35 @@ console.log('DEBUG USER CREATED:', userCreated);
     .json(new ApiResponse(200, userCreated, 'User registered successfully'));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // req body = data 
+  // username or email + password
+  // find the user
+  // compare password
+  // generate access token + refresh token
+  // send cookie 
+
+  const {email, username, password} = req.body;
+
+  if(!email || !username) {
+    throw new ApiError(400, 'Email and Username are required');
+  }
+
+  const user = await User.findOne({
+    $or: [{email}, {username}]
+  });
+
+  if(!user) {
+    throw new ApiError(404, 'User not found');
+  }
+  
+  const ispasswordVaild =  await user.isPasswordCorrect(password);
+if(!ispasswordVaild) {
+    throw new ApiError(401, 'Invalid password');
+}
+
+const {accessToken, refreshToken} = await generateAccessAndRefershToken(user._id);
+
+const loggedInUser = await User
+})
+export { registerUser, loginUser };
