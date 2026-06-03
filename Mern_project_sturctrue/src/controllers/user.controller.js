4e6f60a4d3dd590e6c75917728ecad93cab18a3b
Usmankhalid20 <usmankhalid399354@gmail.com>
2026-06-03 23:47:49 +0500

    change

--- Mern_project_sturctrue/src/controllers/user.controller.js
+++ Mern_project_sturctrue/src/controllers/user.controller.js
@@ -444,7 +444,7 @@ const getWatchHistory = asyncHandler(async(req, res) => {
       }
     }
    ])
-
+console.log("hello")
    return res
    .status(200)
    .json(new ApiResponse(200, user[0].watchHistory), "Watch history fetched successfully")
